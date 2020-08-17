cbe34c4ce3be931a07a991ba9781306c23265be6
nsm5230 <52325901+nsm5230@users.noreply.github.com>
2020-08-17 14:17:01 -0400

    Update and rename zzz.R to msg.R

--- R/zzz.R
+++
@@ -1,4 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-  packageStartupMessage("Welcome to the POL306 Tutorial")
-  packageStartupMessage("If there are no Tutorials listead in the Tutorial tab, close and reopen RStudio")
-}
